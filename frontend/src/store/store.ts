3966f6377294ca2a329d27a2bcf0a1272703bec0
DAPSY-dev <bboydapsy@gmail.com>
2025-01-28 19:05:36 +0200

    Add: Redux devtools

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,7 +1,15 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { thunk } from "redux-thunk";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers =
+  (process.env.NODE_ENV === "development" &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
