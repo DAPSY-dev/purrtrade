a354050a2ecb4b61c564729e038585bb4c8dc91d
DAPSY-dev <bboydapsy@gmail.com>
2025-02-06 23:41:22 +0200

    Update: Home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -11,9 +11,9 @@ function HomePage() {
       <div>
         <h1>Home page</h1>
 
-        <Box className="grid sm:grid-cols-2 gap-4 my-4">
-          <div className="h-15 bg-gray-700 rounded-sm"></div>
-          <div className="h-15 bg-gray-700 rounded-sm"></div>
+        <Box className="grid sm:grid-cols-2 gap-4 mt-4">
+          <div className="h-15 bg-gray-700 rounded-full"></div>
+          <div className="h-15 bg-gray-700 rounded-full"></div>
         </Box>
 
         <div className="pt-4">
