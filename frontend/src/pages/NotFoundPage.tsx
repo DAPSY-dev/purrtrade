a7b46f84444d9236696414484d1874e7f897ad5a
DAPSY-dev <bboydapsy@gmail.com>
2025-02-06 23:22:59 +0200

    Remove: Stack component

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,7 +1,6 @@
 import { useSelector } from "react-redux";
 import SEO from "@/components/SEO";
 import Box from "@/components/Box";
-import Stack from "@/components/Stack";
 import Button from "@/components/Button";
 import { RootState } from "@/store/reducers";
 
@@ -25,24 +24,22 @@ function NotFoundPage() {
     <>
       <SEO title={strings["TITLE_404"]} />
 
-      <Box className="py-12">
-        <Stack gap={2} className="place-items-center">
-          <h1 className="font-bold text-6xl text-gray-700 text-center">
-            {strings["TITLE_404"]}
-          </h1>
+      <Box className="flex flex-col place-items-center gap-2 py-12">
+        <h1 className="font-bold text-6xl text-gray-700 text-center">
+          {strings["TITLE_404"]}
+        </h1>
 
-          <p className="text-2xl text-gray-700 text-center">
-            {strings["OOPS"]} {strings["PAGE_NOT_FOUND"]}
-          </p>
+        <p className="text-2xl text-gray-700 text-center">
+          {strings["OOPS"]} {strings["PAGE_NOT_FOUND"]}
+        </p>
 
-          <p className="text-lg text-gray-700 text-center">
-            {strings["PAGE_MIGHT_BE_MOVED_OR_DELETED"]}
-          </p>
+        <p className="text-lg text-gray-700 text-center">
+          {strings["PAGE_MIGHT_BE_MOVED_OR_DELETED"]}
+        </p>
 
-          <Button as="router-link" to="/" className="mt-4">
-            {strings["GO_BACK_HOME"]}
-          </Button>
-        </Stack>
+        <Button as="router-link" to="/" className="mt-4">
+          {strings["GO_BACK_HOME"]}
+        </Button>
       </Box>
     </>
   );
