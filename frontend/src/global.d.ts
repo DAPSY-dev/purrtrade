d74976a4e99fc1a786a0239c6918f4ef826b3e3e
DAPSY-dev <bboydapsy@gmail.com>
2025-03-08 02:41:43 +0200

    Remove: Popup

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -4,11 +4,6 @@ declare global {
   interface Window {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
   }
-
-  interface HTMLElementEventMap {
-    beforetoggle: ToggleEvent;
-    toggle: ToggleEvent;
-  }
 }
 
 export {};
